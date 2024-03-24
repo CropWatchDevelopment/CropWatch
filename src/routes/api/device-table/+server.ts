3fdaccfc5e8e6fa49d60727d0b4c709a43f8fcdc
Kevin Cantrell <Kevin@CropWatch.io>
2024-03-24 22:33:44 +0900

    test err 2

--- src/routes/api/device-table/+server.ts
+++ src/routes/api/device-table/+server.ts
@@ -1,6 +1,6 @@
 import type { Session, SupabaseClient } from '@supabase/supabase-js';
-import { json, redirect, type RequestEvent } from '@sveltejs/kit'
-import moment from 'moment';
+import { json, redirect } from '@sveltejs/kit'
+// import moment from 'moment';
 
 export async function GET({ locals: { supabase, getSession } }: { locals: { supabase: SupabaseClient, getSession: () => Promise<Session | null> } }) {
     const session = await getSession();
@@ -40,17 +40,17 @@ export async function GET({ locals: { supabase, getSession } }: { locals: { supa
 
         let active = '⚪';
         if (sensor.cw_devices.upload_interval > 0) {
-            if (moment(lastSeen).add(sensor.cw_devices.upload_interval, 'minutes').isAfter(moment().utc())) {
-                active = '🟢';
-            } else {
-                active = '🔴';
-            }
+            // if (moment(lastSeen).add(sensor.cw_devices.upload_interval, 'minutes').isAfter(moment().utc())) {
+            //     active = '🟢';
+            // } else {
+            //     active = '🔴';
+            // }
+            active = '🟢';
         }
 
         const url = sensor.cw_devices.cw_device_type.device_app;
 
         const locationName = Location?.cw_locations?.name ?? 'N/A';
-        console.log(locationName);
 
         return { active, name, locationName, Location, devEui, lastSeen, model, primaryData, url };
     });
@@ -59,13 +59,18 @@ export async function GET({ locals: { supabase, getSession } }: { locals: { supa
 }
 
 async function getAllSensorsForUser(supabase: SupabaseClient, user_id: string) {
-    // const { data, error } = await supabase.from('cw_device_owners').select('*, cw_devices(*, cw_device_locations(*, cw_locations(id, name, latitude, longitude)), cw_device_type(*))').eq('user_id', user_id);
-    const { data, error } = await supabase.from('cw_device_owners').select('*, cw_devices(*, cw_device_locations(id, cw_locations(*)), cw_device_type(*))').eq('user_id', user_id);
-    if (!data) {
+    try {
+        // const { data, error } = await supabase.from('cw_device_owners').select('*, cw_devices(*, cw_device_locations(*, cw_locations(id, name, latitude, longitude)), cw_device_type(*))').eq('user_id', user_id);
+        const { data, error } = await supabase.from('cw_device_owners').select('*, cw_devices(*, cw_device_locations(id, cw_locations(*)), cw_device_type(*))').eq('user_id', user_id);
+        if (!data) {
+            console.error('getAllSensorsForUser', error);
+            return [];
+        } else {
+            return data;
+        }
+    } catch (error) {
         console.error('getAllSensorsForUser', error);
         return [];
-    } else {
-        return data;
     }
 }
 
