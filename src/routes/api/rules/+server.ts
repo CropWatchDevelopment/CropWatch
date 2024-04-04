08fd4929bc0f42ab6651d126c2a2c9a511a2d964
Kevin Cantrell <kevin@cropwatch.io>
2024-04-04 09:29:40 +0900

    test push to master

--- src/routes/api/rules/+server.ts
+++ src/routes/api/rules/+server.ts
@@ -14,16 +14,3 @@ export const POST = async ({request, locals: { supabase, getSession } }) => {
   
   
 }
-
-
-
-// import { json, type RequestEvent } from '@sveltejs/kit'
-
-// export async function POST(event: RequestEvent, { locals: { supabase, getSession }}) {
-//     const session = await getSession();
-//     const form = await event.request.formData();
-
-//     console.log(form, session);
-  
-//     return json({a:1})
-//   }
