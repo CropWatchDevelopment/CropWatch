7ab6c2813d84cf2aecc61e947496f3b28eb4ce55
Kevin Cantrell <kevin@cropwatch.io>
2024-04-22 00:45:33 +0900

    sleeping

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -74,7 +74,6 @@
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
--- src/routes/app/devices/[dev_eui]/data/+page.svelte
+++ src/routes/app/devices/[dev_eui]/data/+page.svelte
@@ -2,12 +2,52 @@
 	import Highcharts from '$lib/actions/highcharts.action';
 
 	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import { HighChartsTimeSeriesChart } from '$lib/charts/highcharts/timeseries';
-	import moment from 'moment';
+	import { supabase } from '$lib/supabaseClient';
 
 	let config: any = [];
+	let currentTemp: number = 0;
+	let currentHumidity: number = 0;
+	let tempData: any = [];
+	let humidityData: any = [];
+
+	const channels = supabase
+		.channel('custom-insert-channel')
+		.on(
+			'postgres_changes',
+			{ event: 'INSERT', schema: 'public', table: 'cw_air_thvd' },
+			(payload) => {
+				console.log('Change received!', payload);
+				if (browser && payload.new.dev_eui === $page.params.dev_eui) {
+					currentHumidity = payload.new.humidity;
+					currentTemp = payload.new.temperatureC;
+					tempData.push([new Date(payload.new.created_at).valueOf(), payload.new.temperatureC]);
+					humidityData.push([new Date(payload.new.created_at).valueOf(), payload.new.humidity]);
+					config = HighChartsTimeSeriesChart(
+						[
+							{
+								type: 'line',
+								yAxis: 0,
+								name: 'Temperature',
+								color: 'red',
+								data: tempData
+							},
+							{
+								type: 'line',
+								yAxis: 1,
+								name: 'Humidity',
+								color: 'blue',
+								data: humidityData
+							}
+						],
+						'Temperature'
+					);
+				}
+			}
+		)
+		.subscribe();
 
 	onMount(async () => {
 		if (browser)
@@ -15,29 +55,45 @@
 				.then((res) => res.json())
 				.then((data) => {
 					console.log(data);
-                    const tempData = data.map((d: any) => [new Date(d.created_at).valueOf(), d.temperatureC]);
-                    const humidityData = data.map((d: any) => [new Date(d.created_at).valueOf(), d.humidity]);
-					config = HighChartsTimeSeriesChart([
-						{
-							type: 'line',
-                            yAxis: 0,
-							name: 'Temperature',
-                            color: 'red',
-							data: tempData,
-						},
-                        {
-							type: 'line',
-                            yAxis: 1,
-							name: 'Humidity',
-                            color: 'blue',
-							data: humidityData,
-						}
-					], 'Temperature');
+					currentHumidity = data[0].humidity;
+					currentTemp = data[0].temperatureC;
+					tempData = data.map((d: any) => [new Date(d.created_at).valueOf(), d.temperatureC]);
+					humidityData = data.map((d: any) => [new Date(d.created_at).valueOf(), d.humidity]);
+					config = HighChartsTimeSeriesChart(
+						[
+							{
+								type: 'line',
+								yAxis: 0,
+								name: 'Temperature',
+								color: 'red',
+								data: tempData
+							},
+							{
+								type: 'line',
+								yAxis: 1,
+								name: 'Humidity',
+								color: 'blue',
+								data: humidityData
+							}
+						],
+						'Temperature'
+					);
 				})
 				.catch((err) => {
 					console.log(err);
 				});
 	});
+
+    onDestroy(() => {
+        if(browser && channels) {
+            channels.unsubscribe();
+        }
+    });
 </script>
 
+<div>
+	<h1>Device Data</h1>
+	<p>Current Temp: {currentTemp}</p>
+	<p>Current Humidity: {currentHumidity}</p>
+</div>
 <div class="chart" use:Highcharts={config} />
