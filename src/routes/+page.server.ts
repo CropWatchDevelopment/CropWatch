c76ab49fda9522b5b19f3541bf86401c098335a4
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2024-04-23 19:04:44 -0600

    updating files to fetch data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,15 @@
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export async function load({ fetch, locals: { supabase, getSession } }) {
+    const session = await getSession();
+    if (!session) throw redirect(304, '/auth/login');
+    const user_id = session?.user.id;
+
+    const deviceTableResponse = await fetch('/api/device-table', {method: 'GET'});
+    const res = await deviceTableResponse.json();
+
+    console.log(res)
+    return {
+        sensors: res,
+    }
+}
