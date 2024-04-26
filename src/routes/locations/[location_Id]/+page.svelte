d5a1994fab3698e3db0c4919456dae080debd464
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2024-04-26 12:29:48 -0600

    location page

--- src/routes/locations/[location_Id]/+page.svelte
+++ src/routes/locations/[location_Id]/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-    import Date from "$lib/components/ui/Date.svelte";
+    import Back from "$lib/components/ui/Back.svelte";
+import Date from "$lib/components/ui/Date.svelte";
+	import NotificationsBell from "$lib/components/ui/NotificationsBell.svelte";
 </script>
-<div class="bg-primary relative">
+<div class="bg-gradient-to-b from-[#132017] to-[#7F8D7F] relative h-screen px-4">
     <Date/>
+    <div class="mt-8 flex justify-between">
+        <Back/>
+        <NotificationsBell/>
+    </div>
 </div>
