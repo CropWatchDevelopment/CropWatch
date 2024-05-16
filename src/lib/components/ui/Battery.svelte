147b9a96606f52cac3012bcd1e7743b659bd3931
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2024-05-15 23:21:36 -0600

    battery component init

---
+++ src/lib/components/ui/Battery.svelte
@@ -0,0 +1,26 @@
+<script lang='ts'>
+    export let battery = 100;
+    function batteryIndicator(value: number){
+        if (value < 30){
+            return 'empty'
+        } else if (value < 70){
+            return 'medium'
+        } else {
+            return 'full'
+        }
+    }
+</script>
+<div>
+    <div class="{batteryIndicator(battery)} w-24 h-24"></div>
+</div>
+<style>
+    .full{
+        background-color: green;
+    }
+    .medium{
+        background-color: yellow;
+    }
+    .empty{
+        background-color: red;
+    }
+</style>
