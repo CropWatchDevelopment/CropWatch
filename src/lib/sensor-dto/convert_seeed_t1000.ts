1b5973b588f8ebb119b18fdf3592c99083455095
Kevin Cantrell <kevin@cropwatch.io>
2024-06-08 21:41:29 +0900

    pre-commit

--- src/lib/sensor-dto/convert_seeed_t1000.ts
+++ src/lib/sensor-dto/convert_seeed_t1000.ts
@@ -1,6 +1,7 @@
 export const SEEED_T1000 = (data: any) => {
     if (!Array.isArray(data)) {
         return {
+            created_at: data.created_at,
             temperatureC: data.temperatureC,
             latitude: data.latitude,
             longitude: data.longitude,
@@ -9,6 +10,7 @@ export const SEEED_T1000 = (data: any) => {
     } else {
         if (!data.length) return null;
         return {
+            // created_at: data[0].created_at,
             temperatureC: data[0].temperatureC,
             latitude: data[0].latitude,
             longitude: data[0].longitude,
