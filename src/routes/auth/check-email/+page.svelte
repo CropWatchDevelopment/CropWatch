8c1a0970a004c769cbb78c113634f41310b63bdf
Kevin Cantrell <kevin@cropwatch.io>
2024-06-11 00:37:05 +0900

    fixed link after email was submitted on registration

--- src/routes/auth/check-email/+page.svelte
+++ src/routes/auth/check-email/+page.svelte
@@ -1,11 +1,51 @@
-<script>
-	import { mdiArrowLeft } from "@mdi/js";
-    import { _ } from 'svelte-i18n';
-	import { Button } from "svelte-ux";
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { Button, TextField } from 'svelte-ux';
+	import { _ } from 'svelte-i18n';
+	import cw_logo from '$lib/images/UI/cropwatch_logo_blue_text.png';
+	import { mdiArrowLeft } from '@mdi/js';
+	import { goto } from '$app/navigation';
+	export let form;
 
+	let email: string = '';
+	let password: string = '';
+	let confirmPassword: string = '';
 </script>
-<div class="flex flex-col w-screen, h-screen justify-center text-center text-5xl">
-    <p>{$_('auth.check_email')}</p>
-    <span class="flex-grow" />
-    <Button href="/auth/login" variant="fill" color="primary" class="w-full" icon={mdiArrowLeft} size="lg">{$_('auth.back_to_login')}</Button>
+
+<div id="login-background">
+	<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+		<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+			<div class="bg-white px-6 py-12 shadow rounded-lg sm:px-12 mx-2 md:mx-0">
+				<div class="sm:mx-auto sm:w-full sm:max-w-md">
+					<img class="mx-auto h-10 w-auto" src={cw_logo} alt="CropWatch" />
+					<h2 class="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+						✉️{$_('auth.check_email')}
+					</h2>
+				</div>
+
+					<Button
+						type="button"
+						variant="fill-outline"
+						color="primary"
+                        icon={mdiArrowLeft}
+                        on:click={() => goto('/auth/login') }
+						classes={{ root: 'w-full' }}>{$_('auth.back_to_login')}</Button
+					>
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	#login-background {
+		transition: opacity 0.3s;
+		opacity: 1;
+		min-height: 100vh;
+		margin: 0;
+		background-attachment: fixed;
+		background-image: url($lib/images/empty-greenhouse.jpg);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center center;
+	}
+</style>
--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -29,14 +29,6 @@ export const actions = {
                     error: "User already exists", email: email, invalid: true, message: "User already exists"
                 })
             }
-            redirect(303, "/auth/check_email");
+            redirect(303, "check-email");
     }
 }
-
-export async function load({locals: { safeGetSession }}) {
-    // const session = await safeGetSession();
-    // // if the user is already logged in return him to the home page
-    // if (session) {
-    //     redirect(303, '/');
-    // }
-  }
