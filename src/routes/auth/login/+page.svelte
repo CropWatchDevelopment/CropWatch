3fde9a15e060575d15430efbae62cb7eb2914b6a
Kevin Cantrell <kevin@cropwatch.io>
2024-06-11 19:28:06 +0900

    updated redirect again

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -24,18 +24,6 @@
 			password = localStorage.getItem('pw') || '';
 		}
 	});
-
-	const getURL = () => {
-		let url =
-			process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-			process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-			'http://localhost:3000/';
-		// Make sure to include `https://` when not localhost.
-		url = url.includes('http') ? url : `https://${url}`;
-		// Make sure to include a trailing `/`.
-		url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-		return url;
-	};
 </script>
 
 <div id="login-background">
@@ -164,7 +152,7 @@
 							supabase.auth.signInWithOAuth({
 								provider: 'google',
 								options: {
-									redirectTo: `${getURL()}/auth/callback`
+									redirectTo: `https://app.cropwatch.io/auth/callback`
 								}
 							});
 						}}
