a211676349cd527b7be8f2694914a93615d89d3c
Kevin Cantrell <kevin@cropwatch.io>
2024-08-22 22:12:46 +0900

    removed server at root

--- src/routes/+server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect, type RequestHandler } from "@sveltejs/kit";
-
-export const GET: RequestHandler = async ({ locals: { safeGetSession } }) => {
-    const session = await safeGetSession();
-    if (!session.user) {
-        throw redirect(303, '/auth/unauthorized');
-    }
-    throw redirect(303, '/app/dashboard');
-}
