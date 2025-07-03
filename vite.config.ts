99a86e9a12f1cc61b93f3c7cea1b819ede95a0ba
Kevin Cantrell <kevin@cropwatch.io>
2025-07-03 11:48:20 +0900

    Revert "adding loggin solution to error handling service"
    This reverts commit 5f30d0de4bf1d6a4c3f55c1d81a39b068a0452f7.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'cropwatch',
-				project: 'cropwatch'
-			}
-		}),
-		tailwindcss(),
-		sveltekit()
-	],
+	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		workspace: [
 			{
