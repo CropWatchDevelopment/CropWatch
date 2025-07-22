1338002e5d4127415c74cd88e12eb176d66d6d7a
Kevin Cantrell <kevin@cropwatch.io>
2025-07-22 14:34:01 +0900

    removed bad vercel file, added config to svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs18.x', // Use Node.js 18 runtime
+			regions: ['hnd1', 'kix1'], // Optional: Specify Vercel regions
+			maxDuration: 300 // Set the maximum duration (in seconds)
+		}),
 		serviceWorker: {
 			register: false // Let PWA plugin handle registration
 		}
